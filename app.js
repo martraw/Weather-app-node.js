24aa649f6c4d0b62ae72f84d0c23f5b5174beb74
Marcin Trawiński <trawinski1@wp.pl>
2018-06-25 21:32:24 +0200

    Add yargs package and encode user input (address) to URI

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for.',
+      string: true
+    }
+    
+  })
+  .help()
+  .alias('help', 'h')
+  .argv
+
+  const addressURI = encodeURIComponent(argv.address)
+  console.log(addressURI);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=56300%20milicz&key=AIzaSyCSo65bZUM2aHihgNNIFpjlox8jpYi4E6g',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressURI}&key=AIzaSyCSo65bZUM2aHihgNNIFpjlox8jpYi4E6g`,
   json: true
 }, (error, response, body) => {
   // console.log(JSON.stringify(response,  undefined, 2));
