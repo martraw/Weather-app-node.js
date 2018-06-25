d3fcd593cf9ff24c1ffb3bc3600f66e64f2d9363
Marcin Trawiński <trawinski1@wp.pl>
2018-06-25 21:53:37 +0200

    Add if else statements in request for error handling

--- app.js
+++ app.js
@@ -9,21 +9,25 @@ const argv = yargs
       describe: 'Address to fetch weather for.',
       string: true
     }
-    
   })
   .help()
   .alias('help', 'h')
   .argv
 
-  const addressURI = encodeURIComponent(argv.address)
-  console.log(addressURI);
+const encodedAddress = encodeURIComponent(argv.address)
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${addressURI}&key=AIzaSyCSo65bZUM2aHihgNNIFpjlox8jpYi4E6g`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCSo65bZUM2aHihgNNIFpjlox8jpYi4E6g`,
   json: true
 }, (error, response, body) => {
   // console.log(JSON.stringify(response,  undefined, 2));
-  console.log(`Miejscowość: ${body.results[0].formatted_address}`);
-  console.log(`Lat: ${body.results[0].geometry.location.lat}`);
-  console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+  if (error) {
+    console.log(`Unable to connect to Google services`);
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log(`Addres ${argv.address} is not valid address`);
+  } else if (body.status === 'OK') {
+    console.log(`Miejscowość: ${body.results[0].formatted_address}`);
+    console.log(`Lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`Lng: ${body.results[0].geometry.location.lng}`);
+  }
 });
